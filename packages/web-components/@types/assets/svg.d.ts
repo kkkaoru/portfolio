37dbd2a3108c9d8c2d7848c16455af1c1a411d8d
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-10 03:24:21 +0900

    Fix type about svg

--- packages/web-components/@types/assets/svg.d.ts
+++ packages/web-components/@types/assets/svg.d.ts
@@ -1,10 +1,3 @@
-// declare module "*.svg" {
-// 	import * as React from "react";
-
-// 	export const ReactComponent: React.FunctionComponent<
-// 		React.SVGProps<SVGSVGElement> & { title?: string }
-// 	>;
-// }
 declare module '*.svg' {
   export const path: string;
   export default path;
