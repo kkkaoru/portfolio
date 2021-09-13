3d7bc72e6d7302f8c28605268ac53c189a641fd6
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-13 20:33:36 +0900

    Update top page

--- packages/web-client/pages/index.tsx
+++ packages/web-client/pages/index.tsx
@@ -1,19 +1,23 @@
 import Head from 'next/head';
-
-import { TwitterLink } from '@web/components/src/components/atoms/link';
+import { Top } from '@web/components/src/components/templates/top';
+import { Icon } from '@/components/next-image';
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Head>
-        <title>Example nextjs, typescript, tailwindcss</title>
-        <meta name="description" content="Example nextjs, typescript, tailwindcss" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>kkkaoru&apos;s site</title>
+        <meta name="description" content="kkkaoru's site. Front end developer." />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
-
       <main>
-        <h1>Web client</h1>
-        <TwitterLink twitterId="kkk4oru" />
+        <Top
+          ImageComponent={Icon}
+          twitterId="kkk4oru"
+          githubId="kkkaoru"
+          contactLink="https://docs.google.com/forms/d/1JJ6KDNZPb9F2nfb1RBZuOuoZZelDARLo0M5s39f6cS0/viewform?edit_requested=true"
+          footerText="&copy;&nbsp;kaoru"
+        />
       </main>
     </div>
   );
