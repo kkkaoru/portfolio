9b1d4aa42c59aaedd584b1c327870d2bc5d7b83c
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-13 20:52:48 +0900

    Add seo tags

--- packages/web-client/pages/index.tsx
+++ packages/web-client/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Top } from '@web/components/src/components/templates/top';
 import { Icon } from '@/components/next-image';
+import { twitterId } from '@/utils/twitter';
 
 export default function Home(): JSX.Element {
   return (
@@ -8,12 +9,17 @@ export default function Home(): JSX.Element {
       <Head>
         <title>kkkaoru&apos;s site</title>
         <meta name="description" content="kkkaoru's site. Front end developer." />
+        <meta property="og:title" content="kkkaoru's site" />
+        <meta property="og:description" content="kkkaoru's site. Front end developer." />
+        <meta property="og:image" content="https://kkkaoru.net/ogp.png" />
+        <meta name="twitter:site" content={`@${twitterId}`} />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <main>
         <Top
           ImageComponent={Icon}
-          twitterId="kkk4oru"
+          twitterId={twitterId}
           githubId="kkkaoru"
           contactLink="https://docs.google.com/forms/d/1JJ6KDNZPb9F2nfb1RBZuOuoZZelDARLo0M5s39f6cS0/viewform?edit_requested=true"
           footerText="&copy;&nbsp;kaoru"
