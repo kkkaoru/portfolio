c04661b043de5488cfe881826f81913b40662044
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-13 20:54:47 +0900

    Fix webcomponent types

--- packages/web-components/stories/templates/top/index.stories.tsx
+++ packages/web-components/stories/templates/top/index.stories.tsx
@@ -9,6 +9,8 @@ export const Default: Story = () => (
   <Top
     twitterId="TwitterJP"
     githubId="github"
+    contactLink="#"
+    footerText="copyright"
     ImageComponent={(args) => (
       <img src="http://placekitten.com/800/800" alt="Cat" width="400" height="400" decoding="async" {...args} />
     )}
