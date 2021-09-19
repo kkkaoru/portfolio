b5f727551c234d50e43226da49baee844a0a83ec
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-20 07:43:59 +0900

    Add random emoji in template

--- packages/web-components/src/components/organisms/header/default.tsx
+++ packages/web-components/src/components/organisms/header/default.tsx
@@ -1,9 +1,11 @@
-import { PartyFace } from '@web-components/components/atoms/logo';
+import { LogoImgWithSvg, LogoProps } from '@web-components/components/atoms/logo';
+import { useRandomEmoji } from '@web-components/utils/emoji/hooks';
 
-export function Header(): JSX.Element {
+export function Header({ ...props }: LogoProps): JSX.Element {
+  const emoji = useRandomEmoji();
   return (
     <header className="pt-4">
-      <PartyFace />
+      <LogoImgWithSvg src={emoji} alt="header logo" {...props} />
     </header>
   );
 }
